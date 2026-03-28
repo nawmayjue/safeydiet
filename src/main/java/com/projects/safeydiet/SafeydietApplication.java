50ae87963f578ebd1bfb4c50a604d602bc5cde9a
1m-C00l <nawmayjue2013@gmail.com>
2026-03-28 10:08:53 +0630

    first commit

---
+++ src/main/java/com/projects/safeydiet/SafeydietApplication.java
@@ -0,0 +1,13 @@
+package com.projects.safeydiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeydietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeydietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/safeydiet/SafeydietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.safeydiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeydietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
